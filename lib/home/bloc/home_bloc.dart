4db150dd4624d25fe6a671b74d5542345defc644
Manuel Jesús España <manespcar23@gmail.com>
2022-04-09 12:30:46 +0200

    * added home page with tests (bloc and widget)

--- lib/home/bloc/home_bloc.dart
+++ lib/home/bloc/home_bloc.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'home_event.dart';
-
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
